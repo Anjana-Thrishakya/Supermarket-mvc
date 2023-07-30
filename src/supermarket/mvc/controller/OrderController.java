a43592485bbb148e8c3a514d11918aa73cc8a452
Anjana Thrishakya <anjanathrishakya@gmail.com>
2023-07-30 16:47:29 +0530

    Order UI add Item

--- src/supermarket/mvc/controller/OrderController.java
+++ src/supermarket/mvc/controller/OrderController.java
@@ -4,10 +4,18 @@
  */
 package supermarket.mvc.controller;
 
+import java.util.ArrayList;
+import supermarket.mvc.model.OrderDetailModel;
+import supermarket.mvc.model.OrderModel;
+
 /**
  *
  * @author anjanathrishakya
  */
 public class OrderController {
+
+    public String placeOrder(OrderModel model, ArrayList<OrderDetailModel> orderDetailModels) {
+        
+    }
     
 }
